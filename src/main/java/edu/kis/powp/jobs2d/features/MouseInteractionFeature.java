937682179f9eba05f8d659172b240ac12835e53c
kw\kamil <251240@edu.p.lodz.pl>
2026-04-30 00:44:35 +0200

    Added MouseInteractionFeature skeleton and registered it in FeaturesManager
    Co-authored-by: 251181 <251181@edu.p.lodz.pl>

---
+++ src/main/java/edu/kis/powp/jobs2d/features/MouseInteractionFeature.java
@@ -0,0 +1,14 @@
+package edu.kis.powp.jobs2d.features;
+
+import edu.kis.powp.appbase.Application;
+
+public class MouseInteractionFeature implements IFeature {
+
+    @Override
+    public void setup(Application application) {}
+
+    @Override
+    public String getName() {
+        return "Mouse Interaction";
+    }
+}
