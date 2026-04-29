56a4e44286a2c3bcc1c2fd897b3d79da46fdddce
kw\kamil <251240@edu.p.lodz.pl>
2026-04-30 00:53:27 +0200

    Added mouse listener
    Co-authored-by: 251181 <251181@edu.p.lodz.pl>

--- src/main/java/edu/kis/powp/jobs2d/features/MouseInteractionFeature.java
+++ src/main/java/edu/kis/powp/jobs2d/features/MouseInteractionFeature.java
@@ -1,11 +1,22 @@
 package edu.kis.powp.jobs2d.features;
 
 import edu.kis.powp.appbase.Application;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JPanel;
 
 public class MouseInteractionFeature implements IFeature {
 
     @Override
-    public void setup(Application application) {}
+    public void setup(Application application) {
+        JPanel panel = application.getFreePanel();
+
+        panel.addMouseListener(new MouseAdapter() {
+
+            @Override
+            public void mouseClicked(MouseEvent e) {}
+        });
+    }
 
     @Override
     public String getName() {
