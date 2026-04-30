89e75f0185644a200dcb3686359cbadd497d373f
Kacper Łukaszewski <251181@edu.p.lodz.pl>
2026-04-30 09:27:58 +0200

    Added javadoc for MouseInteractionListener
    Co-authored-by: 251240 <251240@edu.p.lodz.pl>

--- src/main/java/edu/kis/powp/jobs2d/events/MouseInteractionListener.java
+++ src/main/java/edu/kis/powp/jobs2d/events/MouseInteractionListener.java
@@ -6,16 +6,35 @@ import javax.swing.JPanel;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+/**
+ * Listener handles mouse clicks and passes actions to the driver.
+ * Left click draws, right click moves the device head without drawing.
+ */
 public class MouseInteractionListener extends MouseAdapter {
 
+    /**
+     * Driver access.
+     */
     private final DriverManager driverManager;
+
+    /**
+     * Drawing panel.
+     */
     private final JPanel panel;
 
+    /**
+     * @param driverManager driver provider.
+     * @param panel drawing surface.
+     */
     public MouseInteractionListener(DriverManager driverManager, JPanel panel) {
         this.driverManager = driverManager;
         this.panel = panel;
     }
 
+    /**
+     * Reacts to mouse clicks.
+     * @param e the event to be processed.
+     */
     @Override
     public void mouseClicked(MouseEvent e) {
         if (driverManager.getCurrentDriver() == null) {
