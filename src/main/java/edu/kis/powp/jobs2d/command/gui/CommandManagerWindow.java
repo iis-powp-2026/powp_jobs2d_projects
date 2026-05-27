6ae099c85843dec7e3a33be8ab2820afa967ca09
wstrumiński <251227@edu.p.lodz.pl>
2026-05-27 13:04:12 +0200

    Further refactorization of CommandManagerWindow

--- src/main/java/edu/kis/powp/jobs2d/command/gui/CommandManagerWindow.java
+++ src/main/java/edu/kis/powp/jobs2d/command/gui/CommandManagerWindow.java
@@ -43,56 +43,56 @@ public class CommandManagerWindow extends JFrame implements WindowComponent {
 
         this.commandManager = commandManager;
 
-        GridBagConstraints c = new GridBagConstraints();
+        GridBagConstraints constraint = new GridBagConstraints();
 
         observerListField = new JTextArea("");
         observerListField.setEditable(false);
-        c.fill = GridBagConstraints.BOTH;
-        c.weightx = 1;
-        c.gridx = 0;
-        c.weighty = 0;
-        content.add(observerListField, c);
+        constraint.fill = GridBagConstraints.BOTH;
+        constraint.weightx = 1;
+        constraint.gridx = 0;
+        constraint.weighty = 0;
+        content.add(observerListField, constraint);
         updateObserverListField();
 
         currentCommandField = new JTextArea("");
         currentCommandField.setEditable(false);
-        c.fill = GridBagConstraints.BOTH;
-        c.weightx = 1;
-        c.gridx = 0;
-        c.weighty = 0;
-        content.add(currentCommandField, c);
+        constraint.fill = GridBagConstraints.BOTH;
+        constraint.weightx = 1;
+        constraint.gridx = 0;
+        constraint.weighty = 0;
+        content.add(currentCommandField, constraint);
         updateCurrentCommandField();
 
         previewPanel = new JPanel();
-        c.fill = GridBagConstraints.BOTH;
-        c.weightx = 1;
-        c.gridx = 0;
-        c.weighty = 1;
-        content.add(previewPanel, c);
+        constraint.fill = GridBagConstraints.BOTH;
+        constraint.weightx = 1;
+        constraint.gridx = 0;
+        constraint.weighty = 1;
+        content.add(previewPanel, constraint);
 
         JButton btnImportCommands = new JButton("Import command");
         btnImportCommands.addActionListener((ActionEvent e) -> this.importCommands());
-        c.fill = GridBagConstraints.BOTH;
-        c.weightx = 1;
-        c.gridx = 0;
-        c.weighty = 0;
-        content.add(btnImportCommands, c);
+        constraint.fill = GridBagConstraints.BOTH;
+        constraint.weightx = 1;
+        constraint.gridx = 0;
+        constraint.weighty = 0;
+        content.add(btnImportCommands, constraint);
 
         JButton btnClearCommand = new JButton("Clear command");
         btnClearCommand.addActionListener((ActionEvent e) -> this.clearCommand());
-        c.fill = GridBagConstraints.BOTH;
-        c.weightx = 1;
-        c.gridx = 0;
-        c.weighty = 0;
-        content.add(btnClearCommand, c);
+        constraint.fill = GridBagConstraints.BOTH;
+        constraint.weightx = 1;
+        constraint.gridx = 0;
+        constraint.weighty = 0;
+        content.add(btnClearCommand, constraint);
 
         JButton btnClearObservers = new JButton("Delete observers");
         btnClearObservers.addActionListener((ActionEvent e) -> this.deleteObservers());
-        c.fill = GridBagConstraints.BOTH;
-        c.weightx = 1;
-        c.gridx = 0;
-        c.weighty = 0;
-        content.add(btnClearObservers, c);
+        constraint.fill = GridBagConstraints.BOTH;
+        constraint.weightx = 1;
+        constraint.gridx = 0;
+        constraint.weighty = 0;
+        content.add(btnClearObservers, constraint);
     }
 
     public void initializePreviewPanel(DrawPanelController drawPanelController) {
@@ -160,11 +160,7 @@ public class CommandManagerWindow extends JFrame implements WindowComponent {
     @Override
     public void HideIfVisibleAndShowIfHidden() {
         updateObserverListField();
-        if (this.isVisible()) {
-            this.setVisible(false);
-        } else {
-            this.setVisible(true);
-        }
+        this.setVisible(this.isVisible());
     }
 
 }
